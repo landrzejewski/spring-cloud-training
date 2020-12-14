cd0a10a47520166eb0405947126a373a982809cb
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-12-14 14:34:12 +0100

    payments

--- payments-service/src/main/java/pl/training/cloud/payments/GlobalExceptionHandler.java
+++ payments-service/src/main/java/pl/training/cloud/payments/GlobalExceptionHandler.java
@@ -39,7 +39,8 @@ public class GlobalExceptionHandler {
             description = exceptionName;
         }
         exception.printStackTrace();
-        return ResponseEntity.status(status).body(new ExceptionTransferObject(description, LocalDateTime.now()));
+        return ResponseEntity.status(status)
+                .body(new ExceptionTransferObject(description, LocalDateTime.now()));
     }
 
 }
