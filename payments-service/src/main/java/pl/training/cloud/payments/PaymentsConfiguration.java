488cdbc3dcaf0557c323591f757cef3a2ffd530e
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-12-14 13:47:22 +0100

    payments

---
+++ payments-service/src/main/java/pl/training/cloud/payments/PaymentsConfiguration.java
@@ -0,0 +1,22 @@
+package pl.training.cloud.payments;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class PaymentsConfiguration {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pl.training.cloud.payments"))
+                .build();
+    }
+
+}
