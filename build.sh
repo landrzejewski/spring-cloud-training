5b72c51ce2271027607bee89cc3d4114810e0224
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-12-16 13:52:16 +0100

    day-1

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ docker rmi --force $(docker images | grep 'none')
 docker build -t training/admin-server admin-server
 docker build -t training/configuration-server configuration-server
 docker build -t training/discovery-server discovery-server
+docker build -t training/gateway-server gateway-server
+docker build -t training/payments-service payments-service
+docker build -t training/shop-service shop-service
