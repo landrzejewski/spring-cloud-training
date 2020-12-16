ff3942d413e8a9189f2208e066744b8ba8666b17
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-12-16 11:54:02 +0100

    day-1

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 docker rmi --force $(docker images | grep 'training')
 docker rmi --force $(docker images | grep 'none')
 docker build -t training/admin-server admin-server
+docker build -t training/configuration-server configuration-server
+docker build -t training/discovery-server discovery-server
